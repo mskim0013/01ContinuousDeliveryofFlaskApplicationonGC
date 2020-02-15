f697ab905b43fc7e91122c2212d9eccae63d1a81
mskim0013 <mskim0013@gmail.com>
2020-02-15 17:46:14 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+from flask import Flask
+from flask import jsonify
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello I like to make AI Apps'
+
+@app.route('/name/<value>')
+def name(value):
+    val = {"value": value}
+    return jsonify(val)
+
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
